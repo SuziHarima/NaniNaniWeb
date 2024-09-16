f543eef27a868d4ddff411b7c8a574d3dbe9710a
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-09-16 15:07:33 -0300

    adiciona estruturas para componentes compartilhados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"header",
+        component:FooterComponent
+    }
+];
